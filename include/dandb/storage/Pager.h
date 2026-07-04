4aa5d1fbc83ec317e8ce975369c41a877b32848e
Daniel-AP <daniel24.ap@gmail.com>
2026-07-03 19:04:37 -0600

    feat(storage): add Pager header

---
+++ include/dandb/storage/Pager.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <dandb/core/Status.h>
+#include <dandb/core/Result.h>
+#include <dandb/storage/DiskManager.h>
+#include <dandb/wal/WalManager.h>
+#include <dandb/buffer/BufferPoolManager.h>
+#include <dandb/storage/PageHandle.h>
+#include <dandb/storage/PageId.h>
+#include <dandb/storage/DatabaseHeader.h>
+#include <dandb/platform/DatabasePath.h>
+
+#include <filesystem>
+#include <cstddef>
+
+namespace dandb::storage {
+
+    class Pager {
+        public:
+            Pager(const Pager&) = delete;
+            Pager& operator=(const Pager&) = delete;
+            Pager(Pager&&) noexcept = default;
+            Pager& operator=(Pager&&) noexcept = default;
+
+            static core::Result<Pager> create(std::filesystem::path path, std::size_t bpm_capacity);
+            static core::Result<Pager> open(std::filesystem::path path, std::size_t bpm_capacity);
+
+            core::Result<PageHandle> get_page(PageId page_id);
+            core::Result<PageHandle> new_page();
+
+            core::Status begin_transaction();
+            core::Status commit_transaction();
+            core::Status rollback_transaction();
+
+            core::Status checkpoint();
+            core::Status close();
+
+        private:
+            friend class PageHandle;
+
+            Pager(
+                DiskManager disk_manager,
+                wal::WalManager wal_manager,
+                buffer::BufferPoolManager bpm,
+                platform::DatabasePath path,
+                DatabaseHeader db_header
+            );
+
+            core::Status mark_dirty(PageId page_id);
+
+            DiskManager disk_manager_;
+            wal::WalManager wal_manager_;
+            buffer::BufferPoolManager bpm_;
+            platform::DatabasePath path_;
+            DatabaseHeader db_header_;
+            bool closed_ = false;
+    };
+
+}
